cda708f908f68c7433f286646d2852a9f86a4fdf
Vaibhav Rameshwar Mali <malivaibhav.7620@gmail.com>
2024-04-18 09:31:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+  // Check if the input is 0
+  if (num === 0) {
+    return '0';
+  }
+
+  let binary = ''; // Initialize an empty string to store the binary representation
+
+  while (num > 0) {
+    const remainder = num % 2; // Get the remainder when dividing by 2
+    binary = remainder + binary; // Append the remainder to the beginning of the binary string
+    num = Math.floor(num / 2); // Divide the number by 2
+  }
+
+  return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
